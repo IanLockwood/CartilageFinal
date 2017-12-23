33372a5813fce9036db278f7525c5d136c1cb5af
IanLockwood <mrianlockwood@gmail.com>
2017-12-23 15:06:47 -0800

    Prep for films

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/index'
-  get 'video', to: 'welcome#video'
-  get 'contact', to: 'welcome#contact'
   get 'about', to: 'welcome#about'
 
-  get 'contact-me', to: 'messages#new', as: 'new_message'
-  post 'contact-me', to: 'messages#create', as: 'create_message'
+  get 'contact', to: 'messages#new', as: 'new_message'
+  post 'contact', to: 'messages#create', as: 'create_message'
 
   root 'welcome#index'
 end
